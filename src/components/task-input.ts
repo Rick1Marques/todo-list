c9b886e8609f468a7081823b90da1f4db6db4282
Henrique Marques <henriquemarques@Henriques-Mac-mini.local>
2024-02-22 15:36:05 +0100

    gather data from form

--- src/components/task-input.ts
+++ src/components/task-input.ts
@@ -2,6 +2,7 @@ export class TaskInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+
   constructor() {
     this.templateElement = document.getElementById(
       "inputs"
@@ -13,6 +14,37 @@ export class TaskInput {
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.attach();
+    this.configure();
+  }
+
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const inputData = this.gatherData();
+    console.log(inputData);
+  }
+
+  private gatherData(): [string, string, string, Date] {
+    const titelInput = (document.getElementById("titel")! as HTMLInputElement)
+      .value;
+    const priorityInput = (
+      document.getElementById("priority")! as HTMLInputElement
+    ).value;
+    const descriptionInput = (
+      document.getElementById("description")! as HTMLInputElement
+    ).value;
+    const deadlineInput = (
+      document.getElementById("deadline")! as HTMLInputElement
+    ).value;
+    return [
+      titelInput,
+      priorityInput,
+      descriptionInput,
+      new Date(deadlineInput),
+    ];
   }
 
   private attach() {
